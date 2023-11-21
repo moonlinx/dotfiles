b49dad636d8f3b0979cc9592cc13140efe914152
Devin Delaney <49962728+moonlinx@users.noreply.github.com>
2023-11-21 07:48:34 -0500

    Update .install.sh
    Added more installs

--- .install.sh
+++ .install.sh
@@ -9,4 +9,38 @@ echo "Installing HomeBrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew analytics off
 
-#
+# Install iterm2
+echo "Installing iterm2..."
+brew install --cask iterm2
+
+# Taps
+echo "Tapping Brew..."
+brew tap homebrew/cask-fonts
+brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
+
+# Install brew Formulae
+echo "Installing Brew Formulae..."
+### Essentials
+brew install sketchybar
+brew install skhd
+brew install yabai 
+brew install ifstat
+brew install switchaudio-osx
+brew install ripgrep
+
+### Terminal
+brew install neovim
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+
+### Nice to have
+brew install --cask lulu
+brew install htop
+brew install btop
+
+### Fonts
+brew install --cask sf-symbols
+brew install --cask font-hack-nerd-font
+brew install --cask font-jetbrains-mono
+brew install --cask font-fira-code
