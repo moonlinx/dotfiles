356a9dcf2f3dd6f9ff827a0f7d06e9269606afc2
Devin Delaney <49962728+moonlinx@users.noreply.github.com>
2023-11-27 15:38:24 -0500

    Update .install.sh
    Added more information that I want to install upon new macbook boot

--- .install.sh
+++ .install.sh
@@ -22,12 +22,50 @@ brew tap koekeishiya/formulae
 # Install brew Formulae
 echo "Installing Brew Formulae..."
 ### Essentials
+brew install autoconf
+brew install bat
+brew install curl
+brew instal exa
+brew install fd
+brew install fzf
+brew install gd
+brew install git
+brew install imath
+brew install nano
+brew install neofetch
+brew install neovim
+brew install openssh
+brew install php
+brew install python@3.12
+brew install rust
+brew install six
+brew install spotify-tui
+brew install tree
+brew install zsh
+brew install zoxide
 brew install sketchybar
 brew install skhd
 brew install yabai 
 brew install ifstat
+brew install screenresolution
 brew install switchaudio-osx
 brew install ripgrep
+
+### Casks
+brew install --cask alt-tab
+brew install --cask appcleaner
+brew install --cask espanso
+brew install --cask firefox
+brew install --cask hazel
+brew install --cask iina
+brew install --cask iterm2
+brew install --cask keka
+brew install --cask orion
+brew install --cask raycast
+brew install --cask sf-symbols
+brew install --cask spotify
+brew install --cask vscodium
+
 
 ### Terminal
 brew install neovim
@@ -44,3 +82,8 @@ brew install --cask sf-symbols
 brew install --cask font-hack-nerd-font
 brew install --cask font-jetbrains-mono
 brew install --cask font-fira-code
+
+### Firefox setup
+cd ~/Downloads/
+
+
