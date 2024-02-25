6c21cc316622ad4fdb2fc9de7054b5760e328e64
Devin Delaney <49962728+erihome@users.noreply.github.com>
2024-02-25 08:13:50 -0500

    started install.sh file

---
+++ .install.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+# Install xCode cli tools
+echo "Installing commandline tools..."
+xcode-select --install
+
+# Homebrew
+## Install
+echo "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew analytics off
+
+## Taps
+echo "Tapping Brew..."
+brew tap homebrew/cask-fonts
+brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
+
+# Casks
+echo "Installing Brew Casks..."
+brew install --cask alt-tab
+brew install --cask appcleaner
+brew install --cask espanso
+brew install --cask firefox
+brew install --cask font-hack-nerd-font
+brew install --cask font-symbols-only-nerd-font
+brew install --cask font-ubuntu-mono-nerd-font
+brew install --cask hazel
+brew install --cask iina
+brew install --cask keka
+brew install --cask latest
+brew install --cask lulu
+brew install --cask obsidian
+brew install --cask raycast
+brew install --cask sf-symbols
+brew install --cask spotify
+brew install --cask wezterm
