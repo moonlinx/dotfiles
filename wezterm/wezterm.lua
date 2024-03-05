49c93b5c00c2357c1324a2e19ab7d053d15b9905
Devin Delaney <49962728+erihome@users.noreply.github.com>
2024-03-04 19:05:22 -0500

    Minor changes
    New variables

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ return {
 	enable_tab_bar = false,
 	font_size = 13.0,
 	-- macos_window_background_blur = 40,
-	macos_window_background_blur = 20,
+	macos_window_background_blur = 30,
 
 	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
 	-- window_background_image_hsb = {
@@ -14,7 +14,7 @@ return {
 	-- 	saturation = 0.5,
 	-- },
 	-- window_background_opacity = 0.92,
-	window_background_opacity = 0.70,
+	window_background_opacity = 0.65,
 	-- window_background_opacity = 0.78,
 	-- window_background_opacity = 0.20,
 	window_decorations = "RESIZE",
