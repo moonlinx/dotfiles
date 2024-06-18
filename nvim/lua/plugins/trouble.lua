0398c939eadf0165df51a8578c3a4ec34422bf2c
Devin Delaney <49962728+erihome@users.noreply.github.com>
2024-06-18 08:20:22 -0400

    Nvim Update
    Updated outdated nvim packages

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,6 +1,7 @@
 return {
   "folke/trouble.nvim",
-  branch = "dev", -- IMPORTANT!
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
   keys = {
     {
       "<leader>xx",
@@ -16,6 +17,11 @@ return {
       "<leader>cs",
       "<cmd>Trouble symbols toggle focus=false<cr>",
       desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
     },
     {
       "<leader>xL",
