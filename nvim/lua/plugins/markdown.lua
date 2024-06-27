d31d9e0f69994a3bb69018be33abd82a32674763
Devin Delaney <49962728+erihome@users.noreply.github.com>
2024-06-27 00:28:46 -0400

    NVIM Updates

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,25 +1,11 @@
 return {
   "MeanderingProgrammer/markdown.nvim",
   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter", -- Mandatory
+    "nvim-tree/nvim-web-devicons", -- Optional but recommended
+  },
   config = function()
-    require("render-markdown").setup({
-      headings = {
-        "󰼏 ",
-        "󰼐 ",
-        "󰼑 ",
-        "󰼒 ",
-        "󰼓 ",
-        "󰼔 ",
-      },
-      dash = "━",
-      bullets = { "", "", "", "" },
-
-      checkbox = {
-        unchecked = "󰄰 ",
-        checked = "󰄯 ",
-      },
-      quote = "▍",
-    })
+    require("render-markdown").setup({})
   end,
 }
