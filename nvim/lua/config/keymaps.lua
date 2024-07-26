2f752f27790f462cc177300c96761a3460941991
Devin Delaney <49962728+erihome@users.noreply.github.com>
2024-07-26 10:57:16 -0400

    Nvim Update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,3 +8,5 @@ local keymap = vim.keymap -- for conciseness
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("", ":", ";", { desc = "swap colon with semi colon" })
+keymap.set("", ";", ":", { desc = "swap colon with semi colon" })
