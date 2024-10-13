bdb492f852a2aa364c1d6f37245b5159067f4a70
Devin Delaney <49962728+moonlinx@users.noreply.github.com>
2024-10-13 17:29:04 -0400

    Colorscheme change

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,16 @@
 local wezterm = require("wezterm")
+
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Frappe"]
+custom.background = "#1c1d22"
+custom.tab_bar.background = "#040404"
+custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
+custom.tab_bar.new_tab.bg_color = "#080808"
 return {
 
 	-- Color Scheme
+	color_scheme = {
+		["Catppuccin Frappe"] = custom,
+	},
 	color_scheme = "Catppuccin Frappe",
 
 	-- Tab Bar
@@ -15,7 +24,7 @@ return {
 	macos_window_background_blur = 10,
 
 	-- Opacity
-	window_background_opacity = 0.90,
+	window_background_opacity = 0.80,
 	-- text_background_opacity = 0.20,
 
 	-- Window
