16ca75aeef5d9244101bde01668c5022bc990f3c
Devin Delaney <49962728+moonlinx@users.noreply.github.com>
2024-10-25 12:25:23 -0400

    Changed app icon for zen browser

--- sketchybar/helpers/app_icons.lua
+++ sketchybar/helpers/app_icons.lua
@@ -30,7 +30,8 @@ return {
 	["Anytype"] = ":anytype:",
 	["TeamSpeak 3"] = ":team_speak:",
 	["LibreWolf"] = ":libre_wolf:",
-	["Zen Browser"] = ":zotero:",
+	-- ["Zen Browser"] = ":zotero:",
+	["Zen Browser"] = ":firefox_developer_edition:",
 	["Neovide"] = ":neovide:",
 	["neovide"] = ":neovide:",
 	["Spotlight"] = ":spotlight:",
@@ -217,6 +218,6 @@ return {
 	["Thunderbird"] = ":thunderbird:",
 	["Firefox"] = ":firefox_developer_edition:",
 	["VirusTotal"] = ":default:",
-	["WezTerm"] = ":wezterm:",
+	["WezTerm"] = ":iterm:",
 	["default"] = ":default:",
 }
