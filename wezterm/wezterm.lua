c7d549f44b69e88c4fd360644bad0f112e32f3cc
Devin Delaney <49962728+moonlinx@users.noreply.github.com>
2024-12-17 12:17:26 -0500

    Catppuccin again... of course mocha

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 return {
 
 	-- Color Scheme
-	color_scheme = "Tokyo Night (Gogh)",
+	color_scheme = "Catppuccin Mocha (Gogh)",
 
 	-- Tab Bar
 	enable_tab_bar = false,
@@ -12,13 +12,13 @@ return {
 	line_height = 1.0,
 
 	-- Blur
-	macos_window_background_blur = 30,
+	macos_window_background_blur = 40,
 
 	-- FPS
 	max_fps = 120,
 
 	-- Opacity
-	window_background_opacity = 0.80,
+	window_background_opacity = 0.70,
 	-- text_background_opacity = 0.20,
 
 	-- Window
