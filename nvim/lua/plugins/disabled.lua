c5cf15e8c53dfeffce9127c207cb210f4ea7be27
Fox <fox@fox-den.mynetworksettings.com>
2025-02-23 21:32:36 -0500

    remove markview and markdwon from disabled file

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,7 +2,5 @@ return {
   -- This is the plugin that shows tabs, I don't need it as I use BufExplorer and snipe
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "MeanderingProgrammer/render-markdown.nvim", enabled = true },
-  { "OXY2DEV/markview.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
 }
