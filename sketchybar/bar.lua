ea2a0e182a8b6406733859bd87b4a814fd71b81e
Fox <fox@Fox-Den.local>
2025-04-26 07:46:21 -0400

    Set bar position details

--- sketchybar/bar.lua
+++ sketchybar/bar.lua
@@ -2,8 +2,12 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-	height = 40,
+	height = 39,
+	topmost = "off",
 	color = colors.bar.bg,
-	padding_right = 3,
-	padding_left = 3,
+	padding_right = 2,
+	padding_left = 2,
+	corner_radius = 13,
+	y_offset = 2,
+	margin = 8,
 })
