63a1b601f33a6d33d3a87f12d6c8dec96827adf1
Fox <fox@Fox-Den.local>
2025-04-30 07:45:23 -0400

    Utilize ollama instead of chatgpt for a more local approach

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,15 +1,20 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  lazy = false,
+  version = false, -- Never set this value to "*"! Never!
+  opts = {
+    provider = "ollama",
+    ollama = {
+      model = "llama3.1:8b",
+    },
+  },
   build = "make",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
     "echasnovski/mini.icons",
     {
       -- support for image pasting
@@ -23,30 +28,17 @@ return {
           drag_and_drop = {
             insert_mode = true,
           },
+          -- required for Windows users
+          use_absolute_path = true,
         },
       },
     },
     {
-      -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
         file_types = { "markdown", "Avante" },
       },
       ft = { "markdown", "Avante" },
-    },
-  },
-
-  opts = {
-    -- add any opts here
-    -- for example
-    provider = "claude",
-    claude = {
-      endpoint = "https://api.anthropic.com",
-      model = "claude-3-5-sonnet-20241022", -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- timeout in milliseconds
-      temperature = 0, -- adjust if needed
-      max_tokens = 4096,
-      -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
     },
   },
 }
