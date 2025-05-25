3ee7682f954d3a20f16df4f348e171d872709c18
Fox <fox@Fox-Den.local>
2025-05-25 08:30:59 -0400

    Clean up alias file

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -12,28 +12,14 @@ alias ls="eza --color=always --grid --long --git --no-filesize --icons=always --
 alias la="eza -a"
 alias lt="eza --tree --level=2 --long --icons --git"
 alias vimm="vim"
-alias x="exit"
-alias gn="sudo shutdown -h now"
 alias cat="bat"
-alias z="zoxide"
-alias sv="fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
-alias ya="yazi"
-alias d="delta"
-alias yt="yt-dlp"
-alias f="fastfetch"
-alias sp="spotify_player"
-alias wifi="wifi-password"
 alias py="python3"
+alias pythong="python3"
 alias rm="trash"
 alias ping="ping -c 5"
 alias cp="cp -iv"
 alias mv="mv -iv"
-
-# Sesh
-alias s="./.config/scripts/sesh_start"
-
-# Nmap
-alias nm="nmap -sC -sV -oN nmap-output.txt"
+alias stat="stat -x"
 
 # Dirs
 alias ..="cd .."
