dd60d4c98e214b6903a7383e08c205871709e07e
Fox <fox@Fox-Den.local>
2025-05-28 10:33:46 -0400

    Transparent background is a go

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -31,7 +31,7 @@ return {
       flavour = "mocha", -- Latte, Frappe, Macchiato, Mocha
       no_italic = false,
       term_colors = true,
-      transparent_background = false,
+      transparent_background = true,
       styles = {
         comments = {},
         conditionals = {},
