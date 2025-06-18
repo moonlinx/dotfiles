c828a42b16fea35b88f771cc4afb3b8bdc36d70f
Fox <fox@Fox-Den.local>
2025-06-18 11:12:38 -0400

    Removing AI unused ai tools

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,5 +3,4 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/flash.nvim", enabled = true },
-  { "yetone/avante.nvim", enabled = false },
 }
