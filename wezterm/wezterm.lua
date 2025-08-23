868569262556ac0d7cb0d7759f6d75e6a2c46eab
Fox <fox@Fox-Den.local>
2025-08-23 18:32:09 -0400

    Add rosepine colorscheme to wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -117,6 +117,7 @@ config = {
 	-- color_scheme = "AdventureTime"
 	-- color_scheme = "Catppuccin Frappé (Gogh)",
 	color_scheme = "Catppuccin Mocha (Gogh)",
+	-- color_scheme = "Rosé Pine Moon (Gogh)",
 	-- color_scheme = "Rosé Pine (Gogh)",
 
 	-- Removes the macos bar at the top with the 3 buttons
